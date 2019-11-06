5e3d4e40ed4c0de210f5442871d002f5061386cc
veeck <veeck@neofonie.de>
2019-11-06 14:49:45 +0100

    Add prettier

--- src/components/ClrPikr.vue
+++ src/components/ClrPikr.vue
@@ -1,37 +1,40 @@
 <style lang="less">
-  @selector: .clrpikr;
-  @{selector} {
-    position: relative;
+@selector: .clrpikr;
+@{selector} {
+  position: relative;
 
-    .cp__wrapper {
-      position: absolute;
-      z-index: 1;
-      left: 1px;
-    }
+  .cp__wrapper {
+    position: absolute;
+    z-index: 1;
+    left: 1px;
   }
+}
 </style>
 
 <template>
   <span class="clrpikr">
-    <i v-bind:title="title"
-       class="fas fa-palette"
-       v-bind:class="{
-        active: show === type,
-       }"
-       v-bind:style="{color: clr}"
-       v-on:click="onToggle(badgeKey, type)"></i>
+    <i
+      v-bind:title="title"
+      class="fas fa-palette"
+      v-bind:class="{
+        active: show === type
+      }"
+      v-bind:style="{ color: clr }"
+      v-on:click="onToggle(badgeKey, type)"
+    ></i>
     <color-picker
       v-if="show === type"
       v-bind:color="clr"
-      v-on:change="onChange($event, badgeKey, type)"></color-picker>
+      v-on:change="onChange($event, badgeKey, type)"
+    ></color-picker>
   </span>
 </template>
 
 <script>
 // https://github.com/v-comp/v-color/blob/master/demo.js
-import ColorPicker from '../../node_modules/v-color/dist/index.esm'
+import ColorPicker from "../../node_modules/v-color/dist/index.esm";
 
-const componentName = 'ClrPikr'
+const componentName = "ClrPikr";
 export default {
   name: componentName,
   components: {
@@ -43,18 +46,18 @@ export default {
     },
     title: {
       type: String,
-      default: 'choose color'
+      default: "choose color"
     },
     type: {
       type: String
     },
     show: {
       type: String,
-      default: ''
+      default: ""
     },
     clr: {
       type: String,
-      default: '#CCCCCC'
+      default: "#CCCCCC"
     },
     onToggle: {
       type: Function
@@ -63,5 +66,5 @@ export default {
       type: Function
     }
   }
-}
+};
 </script>
